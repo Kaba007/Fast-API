e851a693ee8e6c59781d6c569653a3cdaed0bd9b
jakub <jakubburianek@gmail.com>
2024-02-25 16:15:46 +0100

    limit for users in the database

--- app/routers/users.py
+++ app/routers/users.py
@@ -14,11 +14,15 @@ router=APIRouter(
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=UserOut)
 def create_user(new_user : UserCreate,db: Session = Depends(get_db)):
-    #hash 
+    #lehké opratření počtu uživatelů
+    user_count  = db.query(models.User).count()
+    if user_count >=10:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"This is only a test project, the number of users in the database is limited to 10")
+    
     user_exists= db.query(models.User).filter(models.User.email == new_user.email).first()
     if user_exists:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"user with email {new_user.email} already exists")
-
+    #hash 
     hashed_password=utils.hash(new_user.password)
     new_user.password=hashed_password
     user= models.User(**new_user.dict())
